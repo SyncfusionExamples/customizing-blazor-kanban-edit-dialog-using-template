3e3e2f59f04e6648c7153a2193a451597e0cdd37
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2023-10-23 13:03:22 +0530

    Update the project

---
+++ Client/KanbanDataModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomKanbanEditDialog
+{
+    public class KanbanDataModel
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Status { get; set; }
+        public string Summary { get; set; }
+       
+        public string Priority { get; set; }
+       
+        public string Assignee { get; set; }
+       
+
+        public List<KanbanDataModel> GetTasks()
+        {
+            List<KanbanDataModel> TaskDetails = new List<KanbanDataModel>();
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 1", Status = "Open", Summary = "Analyze the new requirements gathered from the customer.", Assignee = "Nancy Davloio", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 2", Status = "InProgress", Summary = "Improve application performance", Assignee = "Andrew Fuller", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 3", Status = "Open", Summary = "Arrange a web meeting with the customer to get new requirements.", Assignee = "Janet Leverling", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 4", Status = "InProgress", Summary = "Fix the issues reported in the IE browser.", Assignee = "Janet Leverling", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 5", Status = "Review", Summary = "Fix the issues reported by the customer.", Assignee = "Steven walker", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 6", Status = "Review", Summary = "Fix the issues reported in Safari browser.", Assignee = "Nancy Davloio", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 7", Status = "Close", Summary = "Test the application in the IE browser.", Assignee = "Margaret hamilt", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 8", Status = "Validate", Summary = "Validate the issues reported by the customer.", Assignee = "Steven walker", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 9", Status = "Open", Summary = "Show the retrieved data from the server in grid control.", Assignee = "Margaret hamilt", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 10", Status = "InProgress", Summary = "Fix cannot open user’s default database SQL error.", Assignee = "Janet Leverling", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 11", Status = "Review", Summary = "Fix the issues reported in data binding.", Assignee = "Janet Leverling", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 12", Status = "Close", Summary = "Analyze SQL server 2008 connection.", Assignee = "Andrew Fuller", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 13", Status = "Validate", Summary = "Validate databinding issues.", Assignee = "Margaret hamilt", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 14", Status = "Close", Summary = "Analyze grid control.", Assignee = "Margaret hamilt", Priority = "Low"  });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 15", Status = "Close", Summary = "Stored procedure for initial data binding of the grid.", Assignee = "Steven walker", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 16", Status = "Close", Summary = "Analyze stored procedures.", Assignee = "Janet Leverling", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 17", Status = "Validate", Summary = "Validate editing issues.", Assignee = "Nancy Davloio", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 18", Status = "Review", Summary = "Test editing functionality.", Assignee = "Nancy Davloio", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 19", Status = "Open", Summary = "Enhance editing functionality.", Assignee = "Andrew Fuller", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 20", Status = "InProgress", Summary = "Improve the performance of the editing functionality.", Assignee = "Nancy Davloio", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 21", Status = "Open", Summary = "Arrange web meeting with the customer to show editing demo.", Assignee = "Steven walker", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 22", Status = "Review", Summary = "Fix the editing issues reported by the customer.", Assignee = "Janet Leverling", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 23", Status = "Testing", Summary = "Fix the issues reported by the customer.", Assignee = "Steven walker", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 24", Status = "Testing", Summary = "Fix the issues reported in Safari browser.", Assignee = "Nancy Davloio", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 25", Status = "Testing", Summary = "Fix the issues reported in data binding.", Assignee = "Janet Leverling", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 26", Status = "Testing", Summary = "Test editing functionality.", Assignee = "Nancy Davloio", Priority = "Low" });
+            TaskDetails.Add(new KanbanDataModel { Id = "Task 27", Status = "Testing", Summary = "Test editing feature in the IE browser.", Assignee = "Janet Leverling", Priority = "Low" });
+            return TaskDetails;
+        }
+
+        public List<KanbanDataModel> GetTaskProgress()
+        {
+            List<KanbanDataModel> ProgressDetails = new List<KanbanDataModel>();
+            ProgressDetails.Add(
+                new KanbanDataModel { 
+                    Id = "0", 
+                    Status = "Open", 
+                    Assignee = "Nancy Davloio", 
+                    Priority = "Low" 
+                });
+            ProgressDetails.Add(
+                new KanbanDataModel { 
+                    Id = "1",
+                    Status = "InProgress",
+                    Assignee = "Andrew Fuller",
+                    Priority = "Normal" 
+                });
+            ProgressDetails.Add(
+                new KanbanDataModel { 
+                    Id = "2", 
+                    Status = "Testing", 
+                    Assignee = "Janet Leverling", 
+                    Priority = "Critical" 
+                });
+            ProgressDetails.Add(
+                new KanbanDataModel { 
+                    Id = "3", 
+                    Status = "Close", 
+                    Assignee = "Steven walker", 
+                    Priority = "Release Breaker" });
+            return ProgressDetails;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # Customizing Blazor Kanban Edit Dialog using Templates
+
 A quick-start Blazor project that shows how to customize the edit dialog in the Syncfusion Blazor Kanban component using template. This project also contains code snippet to perform CRUD operations in the Kanban board.
 
-Refer to the following documentation to learn about dialog editing in the Blazor Kanban component: 
-https://blazor.syncfusion.com/documentation/kanban/dialog#dialog-template
+**Dialog Template**: https://blazor.syncfusion.com/documentation/kanban/dialog#dialog-template
 
-Check out this online example of the Blazor Kanban dialog editing:
-https://blazor.syncfusion.com/demos/kanban/dialog-editing?theme=bootstrap5
+**Online Sample**: https://blazor.syncfusion.com/demos/kanban/dialog-editing?theme=bootstrap5
 
-## Project prerequisites
-Make sure that you have the compatible versions of [Visual Studio 2019 (version 16.6+)]( https://visualstudio.microsoft.com/downloads?utm_source=github&utm_medium=listing&utm_campaign=blazor-gantt-chart-github-samples) and [.NET Core SDK 3.1.8](https://dotnet.microsoft.com/download/dotnet-core/3.1?utm_source=github&utm_medium=listing&utm_campaign=blazor-gantt-chart-github-samples) or later version in your machine before starting to work on this project.
+## Prerequisites
 
-## How to run this application
-To run this application, you need to first clone the `customizing-blazor-kanban-edit-dialog-using-template` repository and then open it in Visual Studio 2019. Now, simply build and run your project to view the output.
+* Visual Studio 2022
 
+## How to run the project
+
+* Checkout this project to a location in your disk.
+* Open the solution file using the Visual Studio 2022.
+* Restore the NuGet packages by rebuilding the solution.
+* Run the project.
